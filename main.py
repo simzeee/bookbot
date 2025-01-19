5f35d494949ce17b5f83f6427d5deabe26399b68
simzeee <jwsims816@gmail.com>
2025-01-19 16:21:11 -0500

    finish report

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ def main():
         file_contents = f.read()
         # print(file_contents)
         words = file_contents.split()
-        # number_of_words = len(words)
-        word_count = count_words(file_contents)
-        print(word_count)
+        number_of_words = len(words)
+        character_count = count_characters(file_contents)
+        # print(character_count)
+        final_report = print_report(character_count, number_of_words)
+        print(final_report)
 
 
-def count_words(all_words):
+def count_characters(all_words):
     count_dict = {}
     for s in all_words:
         lower_s = s.lower()
@@ -18,6 +20,27 @@ def count_words(all_words):
         else:
             count_dict[lower_s] = 1
     return count_dict
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
+def print_report(character_count, word_count):
+    report = "--- Begin report of books/frankenstein.txt ---\n"
+    report += f"{word_count} words found in the document\n"
+    result = [{key: value} for key, value in character_count.items()]
+    filtered_sorted_data = sorted(
+        (d for d in result if list(d.keys())[0].isalpha()),
+        key=lambda d: list(d.values())[0],
+        reverse=True,
+    )
+    # print(filtered_sorted_data)
+    for character_count in filtered_sorted_data:
+        (key,) = character_count.keys()
+        report += f"\nThe '{key}' character was found {character_count[key]} times"
+    report += "\n--- End report ---"
+    return report
 
 
 main()
